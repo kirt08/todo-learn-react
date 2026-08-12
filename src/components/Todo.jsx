1d9c4c72926a7747d65003b7bd5b9e0cd9823245
romanov.da <dimromgomell@gmail.com>
2026-08-12 11:14:22 +0300

    lesson7, add storaging with localStorage

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,42 +1,83 @@
+import { useState, useEffect } from "react"
+
 import AddTaskForm from "./AddTaskForm"
 import SearchTaskForm from "./SearchTaskForm"
 import TodoInfo from "./TodoInfo"
 import TodoList from "./TodoList"
 
 const Todo = () => {
-    const tasks = [
-        { id: "task-1", title: "Buy Milk", isDone: false },
-        { id: "task-2", title: "Pet the cat", isDone: true },
-    ]
+    const [tasks, setTasks] = useState(() => {
+        const savedTasks = localStorage.getItem("tasks")
+
+        return savedTasks ? JSON.parse(savedTasks) : [
+            { id: "task-1", title: "Buy Milk", isDone: false },
+            { id: "task-2", title: "Pet the cat", isDone: true },
+        ]
+    })
+
+    const [newTaskTitle, setNewTaskTitle] = useState("")
+    const [searchQuery, setSearchQuery] = useState("")
 
     const deleteAllTasks = () => {
-        console.log("Delete all tasks!")
+        const isConfirmed = confirm("Are you sure you want to delete all tasks")
+
+        if (isConfirmed) {
+            setTasks([])
+        }
     }
 
     const deleteTask = (taskId) => {
-        console.log(`Delete task with id = ${taskId}`)
+        setTasks(
+            tasks.filter((task) => task.id !== taskId)
+        )
     }
 
     const toggleTaskComplete = (taskId, isDone) => {
-        console.log(`Task ${taskId} ${isDone ? "done" : "not done"}`)
-    }
+        setTasks(
+            tasks.map((task) => {
+                if (task.id === taskId) {
+                    return { ...task, isDone}
+                }
 
-    const filterTasks = (query) => {
-        console.log(`Поиск: ${query}`)
+                return task
+            })
+        )
     }
 
     const addTask = () => {
-        console.log("Task added!")
+        if (newTaskTitle.trim().length > 0) {
+            const newTask = {
+                id: crypto?.randomUUID() ?? Date.now().toString(),
+                title: newTaskTitle,
+                isDone: false,
+            }
+
+            setTasks([...tasks, newTask])
+            setNewTaskTitle("")
+            setSearchQuery("")
+        }
     }
+
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+    }, [tasks])
+
+    const clearSearchQuery = searchQuery.trim().toLowerCase()
+    const filteredTasks = clearSearchQuery.length > 0
+        ? tasks.filter(({ title }) => title.toLowerCase().includes(clearSearchQuery))
+        : null
 
     return (
         <div className="todo">
             <h1 className="todo__title">To Do List</h1>
             <AddTaskForm 
                 addTask={addTask}
+                newTaskTitle={newTaskTitle}
+                setNewTaskTitle={setNewTaskTitle}
             />
             <SearchTaskForm 
-                onSearchInput={filterTasks}
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
             />
             <TodoInfo 
                 total = {tasks.length}
@@ -45,6 +86,7 @@ const Todo = () => {
             />
             <TodoList 
                 tasks={tasks}
+                filteredTasks={filteredTasks}
                 onDeleteTaskButtonClick={deleteTask}
                 onTaskCompleteChange={toggleTaskComplete}
             />
