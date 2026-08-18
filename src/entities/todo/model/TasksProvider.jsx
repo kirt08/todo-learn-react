e03bd2f2a2322a53105f2fcb9f64070f71cbe66e
romanov.da <dimromgomell@gmail.com>
2026-08-18 18:04:05 +0300

    lesson19: add tasksReducer

--- src/entities/todo/model/TasksProvider.jsx
+++ src/entities/todo/model/TasksProvider.jsx
@@ -1,6 +1,7 @@
 import { TasksContext } from "./TasksContext.js"
 import useTasks from "./useTasks.js"
 import useIncompleteTasksScroll from "./useIncompleteTaskScroll.js"
+import { useMemo } from "react"
 
 export const TasksProvider = (props) => {
     const { children } = props
@@ -26,26 +27,43 @@ export const TasksProvider = (props) => {
         firstIncompleteTaskId,
     } = useIncompleteTasksScroll(tasks)
 
+    const value = useMemo(() => ({
+        tasks,
+        filteredTasks,
+        deleteTask,
+        deleteAllTasks,
+        toggleTaskComplete,
+        newTaskTitle,
+        setNewTaskTitle,
+        searchQuery,
+        setSearchQuery,
+        newTaskInputRef,
+        addTask,
+        disappearingTaskId,
+        appearingTaskId,
+        firstIncompleteTaskRef,
+        firstIncompleteTaskId,
+    }), [
+        tasks,
+        filteredTasks,
+        deleteTask,
+        deleteAllTasks,
+        toggleTaskComplete,
+        newTaskTitle,
+        setNewTaskTitle,
+        searchQuery,
+        setSearchQuery,
+        newTaskInputRef,
+        addTask,
+        disappearingTaskId,
+        appearingTaskId,
+        firstIncompleteTaskRef,
+        firstIncompleteTaskId,
+    ])
+
     return (
         <TasksContext.Provider
-            value={{
-                tasks,
-                filteredTasks,
-                firstIncompleteTaskRef,
-                firstIncompleteTaskId,
-                deleteTask,
-                deleteAllTasks,
-                toggleTaskComplete,
-
-                newTaskTitle,
-                setNewTaskTitle,
-                searchQuery,
-                setSearchQuery,
-                newTaskInputRef,
-                addTask,
-                disappearingTaskId,
-                appearingTaskId,
-            }}
+            value={value}
         >
             {children}
         </TasksContext.Provider>
