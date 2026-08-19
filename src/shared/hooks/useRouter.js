e3e22919e5d2c73c2ba1e18913c71e9c90482da2
romanov.da <dimromgomell@gmail.com>
2026-08-19 13:59:17 +0300

    lesson22: finish steps

--- src/shared/hooks/useRouter.js
+++ src/shared/hooks/useRouter.js
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react"
+import { BASE_URL } from "@/shared/constants"
+
+const getCurrentPath = () => {
+    const pathname = window.location.pathname
+
+    return pathname.startsWith(BASE_URL)
+        ? pathname.slice(BASE_URL.length - 1) || "/"
+        : pathname
+}
 
 export const useRoute = () => {
-    const [path, setPath] = useState(window.location.pathname)
+    const [path, setPath] = useState(getCurrentPath())
 
     useEffect(() => {
         const onLocationChange = () => {
-            setPath(window.location.pathname)
+            setPath(getCurrentPath())
         }
 
         window.addEventListener("popstate", onLocationChange)
